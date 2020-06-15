27b685e3ee4d1632f91794b882977b4319f6a4f0
cristian robles <gogo90553@icloud.com>
2020-06-14 22:50:05 -0500

    Navbar done just some bugs with the router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Nav from "./nav.js";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <div>
+        <Nav/>
+        {/* <img src={.Assets/Pizza.jpg} alt="home-picture" /> */}
+      </div>
     </>
   );
 };
